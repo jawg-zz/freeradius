adbe4b7a0079e86aac08d7779a1c3a7dc4c02b58
Mustafa Sahin <nullsoft8411@gmail.com>
2023-07-25 12:29:04 +0200

    start services only in run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,15 @@
 # Executable process script for Freeradius + DaloRadius + MySQL image:
 # GitHub: git@github.com:nullsoft8411/freeradius.git
 
+service mysql start
+
+# wait for MySQL-Server to be ready
+echo -n "Waiting for mysql ($MYSQL_HOST)..."
+while ! mysqladmin ping -h"$MYSQL_HOST" -p"$MYSQL_PASSWORD" --silent; do
+    sleep 20
+done
+echo "ok"
+
 INIT_LOCK=/data/.init_done
 
 if test -f "$INIT_LOCK"; then
@@ -12,16 +21,8 @@ if test -f "$INIT_LOCK"; then
     echo "Init lock file exists and config file exists, skipping initial setup of daloRADIUS."
 else
     /var/www/daloradius/init.sh
-    date >$INIT_LOCK
 fi
 
-service mysql start
-# wait for MySQL-Server to be ready
-echo -n "Waiting for mysql ($MYSQL_HOST)..."
-while ! mysqladmin ping -h"$MYSQL_HOST" -p"$MYSQL_PASSWORD" --silent; do
-    sleep 20
-done
-echo "MySQL - Done"
 service apache2 start
 service freeradius start
 #/usr/sbin/freeradius -X
