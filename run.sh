c8b6be2fe1de0270ecd30485f3b16e2449c7c28d
Mustafa Sahin <nullsoft8411@gmail.com>
2023-07-25 14:11:02 +0200

    improve check for first run

--- run.sh
+++ run.sh
@@ -9,20 +9,22 @@ service mysql start
 # wait for MySQL-Server to be ready
 echo -n "Waiting for mysql ($MYSQL_HOST)..."
 while ! mysqladmin ping -h"$MYSQL_HOST" -p"$MYSQL_PASSWORD" --silent; do
-    sleep 20
+        sleep 20
 done
 echo "ok"
 
 INIT_LOCK=/data/.init_done
 
-if test -f "$INIT_LOCK"; then
-    if ! test -f "$DALORADIUS_CONF_PATH" || ! test -s "$DALORADIUS_CONF_PATH"; then
-        echo "Init lock file exists but config file does not exist or is 0 bytes, performing initial setup of daloRADIUS."
-        /var/www/daloradius/init.sh
-    fi
-    echo "Init lock file exists and config file exists, skipping initial setup of daloRADIUS."
+if [ -e "$INIT_LOCK" ]; then
+        if [ ! -e "$DALORADIUS_CONF_PATH" ] || [ ! -s "$DALORADIUS_CONF_PATH"]; then
+                echo "Init lock file exists but config file does not exist or is 0 bytes, performing initial setup of daloRADIUS."
+                /var/www/daloradius/init.sh
+                date >$INIT_LOCK
+        fi
 else
-    /var/www/daloradius/init.sh
+        echo "init lock file not exsist. run first time init"
+        /var/www/daloradius/init.sh
+        date >$INIT_LOCK
 fi
 
 service apache2 start
