5d7926b6e4650a60ccd1d67b75d895d499bd2eec
jawg-zz <mosetigeorge@gmail.com>
2024-09-09 06:44:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
-# Start script for Freeradius + DaloRadius + MySQL
+# Main startup script for Freeradius + DaloRadius + MySQL setup
 
+echo "Starting Freeradius + daloRADIUS + MySQL setup"
+
+# Log current date and time
 date +"%d-%m-%y - %H:%M"
-
-# Start MySQL service
-service mysql start
-
-# Wait for MySQL to be ready
-echo -n "Waiting for mysql ($MYSQL_HOST)..."
-while ! mysqladmin ping -h"$MYSQL_HOST" -p"$MYSQL_PASSWORD" --silent; do
-    sleep 20
-done
-echo "ok"
-
-# Check if initialization is needed
-INIT_LOCK=/data/.init_done
-
-if [ -e "$INIT_LOCK" ]; then
-    echo "Init lock file exists"
-    if [ ! -e "$DALORADIUS_CONF_PATH" ] || [ ! -s "$DALORADIUS_CONF_PATH" ]; then
-        echo "Config file does not exist or is 0 bytes, performing initial setup of daloRADIUS."
-        /var/www/daloradius/init.sh
-        date > $INIT_LOCK
-    fi
-else
-    echo "Init lock file does not exist. Running first-time initialization."
-    /var/www/daloradius/init.sh
-    date > $INIT_LOCK
-fi
 
 # Start Apache
 service apache2 start
 
-# Start Freeradius in debug mode
-/usr/sbin/freeradius -X
+# Start FreeRADIUS
+service freeradius stop
+echo "Starting FreeRADIUS in debug mode..."
+/usr/sbin/freeradius -X &
+
+# Wait for MySQL Server to be ready
+echo -n "Waiting for external MariaDB ($MYSQL_HOST)..."
+while ! mysqladmin ping -h"$MYSQL_HOST" -p"$MYSQL_PASSWORD" --silent; do
+    sleep 5
+done
+echo "MariaDB is ready."
+
+# Initialize if required
+INIT_LOCK=/data/.init_done
+
+if [ -e "$INIT_LOCK" ]; then
+    echo "Initialization already done, skipping..."
+else
+    echo "First-time setup: Running init script..."
+    /var/www/daloradius/init.sh
+    date > "$INIT_LOCK"
+    echo "Initialization completed."
+fi
+
+# Keep FreeRADIUS and Apache running in the foreground
+echo "Services started. Keeping container running."
+tail -f /var/log/apache2/access.log /var/log/freeradius/radius.log
