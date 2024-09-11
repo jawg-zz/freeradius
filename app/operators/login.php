de36d185a23f33595b92c2488bd7547fbd94fe09
jawg-zz <mosetigeorge@gmail.com>
2024-09-12 01:39:41 +0300

    revert changes

--- app/operators/login.php
+++ app/operators/login.php
@@ -24,8 +24,8 @@
 include_once("library/sessions.php");
 dalo_session_start();
 
-if (!array_key_exists('daloradius_logged_in', $_SESSION)
-    && $_SESSION['daloradius_logged_in'] == false) {
+if (array_key_exists('daloradius_logged_in', $_SESSION)
+    && $_SESSION['daloradius_logged_in'] !== false) {
     header('Location: index.php');
     exit;
 }
